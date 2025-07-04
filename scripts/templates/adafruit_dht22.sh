bb2c5f5979fa938e7e847d72edc2507af41f7c02
BECATRUE <jaehun0121@snu.ac.kr>
2025-07-04 21:33:30 +0900

    Add template for scripts
    See also: #3

---
+++ scripts/templates/adafruit_dht22.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Move to your project path
+cd /path/to/your/project
+
+# Set environment variables (if needed)
+
+# Measure and notify
+python -m rpi_sensor_log.monitor \
+    --google-sheets-url "https://docs.google.com/spreadsheets/d/YOUR_GOOGLE_SHEETS_ID/edit" \
+    --google-sheets-worksheet-name "YOUR_WORKSHEET_NAME" \
+    adafruit_dht22 \
+    --pin-number YOUR_GPIO_PIN_NUMBER \
